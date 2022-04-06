1c286471f344946ff0aae27d2873df04accb8d6e
Mateusz Stobiecki <m100biecki@gmail.com>
2022-04-06 19:18:01 +0200

    complete footer and scroll on hamburger navigation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-const hamburgerButton = document.querySelector(".hamburger");
-const navigationMobile = document.querySelector(".navigation__items--mobile");
-const navigation = document.querySelector(".navigation__items");
-const navigationLink = document.querySelectorAll(".navigation__items-link");
+const hamburgerButton = document.querySelector('.hamburger');
+const navigationMobile = document.querySelector('.navigation__items--mobile');
+const navigation = document.querySelector('.navigation__items');
+const navigationLink = document.querySelectorAll('.navigation__items-link');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
-	hamburgerButton.classList.toggle("is-active");
-	navigationMobile.classList.toggle("navigation__items--mobile--active");
+	hamburgerButton.classList.toggle('is-active');
+	navigationMobile.classList.toggle('navigation__items--mobile--active');
 
 	navigationLink.forEach((item) => {
-		item.addEventListener("click", () => {
-			navigation.classList.remove("navigation__items--mobile--active");
-			hamburgerButton.classList.remove("is-active");
+		item.addEventListener('click', () => {
+			navigation.classList.remove('navigation__items--mobile--active');
+			hamburgerButton.classList.remove('is-active');
 		});
 	});
 };
-hamburgerButton.addEventListener("click", handleNav);
+hamburgerButton.addEventListener('click', handleNav);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
